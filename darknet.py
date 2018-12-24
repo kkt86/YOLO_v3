2c1f58d24f9029415f1ee161c354f652192a2889
Konstantin Palagachev <konstantin.palagachev@gmail.com>
2018-12-24 11:39:33 +0100

    finished building the Yolo architecture

--- darknet.py
+++ darknet.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from pprint import pprint
 
+
 def parse_cfg(cfgfile):
     """
     Takes a configuration file
@@ -36,4 +37,108 @@ def parse_cfg(cfgfile):
     return blocks
 
 
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
 
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.achors = anchors
+
+
+def create_modules(blocks):
+    net_info = blocks[0]
+    module_list = nn.ModuleList()
+    prev_filters = 3  # keeps track of previous filters, initialized with RGB filters
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+
+        # check the type of block
+        # create a new module for the block
+        # append to module_list
+        if x['type'] == 'convolutional':
+            activation = x['activation']
+            try:
+                batch_normalize = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+            pad = (kernel_size - 1) // 2 if padding else 0
+
+            # add the convolutional layer
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module("conv_{0}".format(index), conv)
+
+            # add the batch normalization layer
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            # check activation (either linear or Leaky ReLU
+            if activation == 'leaky':
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module('leaky_{0}'.format(index), activn)
+
+        elif x['type'] == 'upsample':
+            stride = int(x['stride'])
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+            module.add_module('upsample_{0}'.format(index), upsample)
+
+        elif x['type'] == 'route':
+            x['layers'] = x['layers'].split(',')
+            # start of a route
+            start = int(x['layers'][0])
+            # end, if there exists one
+            try:
+                end = int(x['layers'][1])
+            except:
+                end = 0
+            # positive annotation
+            start = start - index if start > 0 else start
+            end = end - index if end > 0 else end
+
+            route = EmptyLayer()
+            module.add_module('route_{0}'.format(index), route)
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters = output_filters[index + start]
+
+        # shortcut corresponding to skip connection
+        elif x['type'] == 'shortcut':
+            shortcut = EmptyLayer()
+            module.add_module('shortcut_{0}'.format(index), shortcut)
+
+        # Yolo is the detection layer
+        elif x['type'] == 'yolo':
+            mask = x['mask'].split(',')
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module('detection_{0}'.format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
+
+
+if __name__ == '__main__':
+    blocks = parse_cfg('cfg/yolov3.cfg')
+    print(create_modules(blocks))
